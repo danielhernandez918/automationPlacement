1faa7d028957ea3e08181c9c84cddb98152d14a4
Daniel Hernandez <danielhernandez918@gmail.com>
2022-06-21 15:46:21 -0700

    update

--- autoPlacement.py
+++ autoPlacement.py
@@ -7,25 +7,16 @@ from watchdog.events import FileSystemEventHandler
 
 
 class MyHandler(FileSystemEventHandler):
-    # i = 1
 
     def on_modified(self, event):
-        # new_name = "new_file_" + str(self.i) + ".pdf"
         for filename in os.listdir(folder_to_track):
-            # file_exists = os.path.isfile(folder_destination + "/" + new_name)
-            # while file_exists:
-            #     self.i += 1
-            #     new_name = "new_file_" + str(self.i) + ".pdf"
-            #     file_exists = os.path.isfile(folder_destination + "/" + new_name)
-
             src = folder_to_track + "/" + filename
             new_destination = folder_destination + "/" + filename
-            # new_destination = folder_destination + "/" + new_name
             os.rename(src, new_destination)
 
 
-folder_to_track = "/Users/danie/OneDrive/Pictures"
-folder_destination = "/Users/danie/OneDrive/Documents/2022_Pictures"
+folder_to_track = "/Users/danie/OneDrive/Downloads"
+folder_destination = "/Users/danie/OneDrive/Documents/2022_PDFs"
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
